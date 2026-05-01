bb4020fa5df78f6d95a99837fac426579ed0677a
RyanHandhika <muhammadryanhandhikasusantorya@gmail.com>
2026-05-01 13:47:48 +0700

    bug/prisma integrated

---
+++ src/db/prisma.js
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error(
+    "DATABASE_URL is missing. Check your .env file and dotenv/config import.",
+  );
+}
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
